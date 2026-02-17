0430d59959761d1f050322e73a198062f48f46dd
Sandeep-XD <sanbofficial@gmail.com>
2026-02-17 15:45:17 +0530

    spring project initialized

---
+++ hybridAuth/src/test/java/in/bank/hdfc/auth/hybridAuth/HybridAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package in.bank.hdfc.auth.hybridAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
